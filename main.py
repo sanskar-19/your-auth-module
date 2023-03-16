ca499ceac38dda25a04b9a2aa03acb8b6514094e
Sanskar <sanskaaragrawal2001@gmail.com>
2023-03-16 20:43:07 +0530

    Second commit

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == "__main__":
+
     # defining the entry point for our server
-    uvicorn.run("server.app:app",host=os.getenv("HOST"),port=int(os.getenv("PORT")),reload=True)
+    uvicorn.run(
+        "server.app:app",
+        host=os.getenv("HOST"),
+        port=int(os.getenv("PORT")),
+        reload=True,
+    )
