e5178b5b5dca8b3670b79ede50c290e392cd98f7
Sanskar <sanskaaragrawal2001@gmail.com>
2023-03-19 15:37:40 +0530

    Adding token validation

--- server/utils/jwt.py
+++ server/utils/jwt.py
@@ -1,8 +1,8 @@
 import jwt
+import time
+from datetime import datetime, timedelta
 from cryptography.hazmat.primitives import serialization
-
-# pu = load_public_key(pk_file_path=PUBLIC_KEY)
-# pr = load_private_key(pk_file_path=PRIVATE_KEY)
+from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
 
 
 def load_private_key(pk_file_path: str, password: str | None = ""):
@@ -21,14 +21,35 @@ def load_public_key(pk_file_path: str):
     return serialization.load_ssh_public_key(public_key.encode())
 
 
+pu = load_public_key(pk_file_path="server\\utils\\keys\\id_rsa.pub")
+pr = load_private_key(pk_file_path="server\\utils\\keys\\id_rsa")
+
+
 def create_access_token(userid: str, email: str, role: str | None):
     payload = {
         "uid": userid,
         "email": email,
         "role": role,
+        "iat": datetime.utcnow(),
+        "exp": datetime.utcnow() + timedelta(seconds=30),
     }
-    return jwt.encode(payload, "my-secret-key", "HS256")
+
+    return jwt.encode(payload, key=pr, algorithm="RS256")
 
 
 def validate_access_token(access_token: str):
-    pass
+    try:
+        payload = jwt.decode(access_token, key=pu, algorithms="RS256")
+
+        if payload["exp"] <= time.time():
+            raise ExpiredTokenError("Token Expired")
+
+        return payload
+
+    except Exception as e:
+        raise e
+
+
+class ExpiredTokenError(Exception):
+    def __init__(self, msg):
+        Exception(msg)
